22d472870f39f7cbcae934c70fc0de7ef09d7397
Toan Thang <box.dota@gmail.com>
2023-08-15 02:09:06 +0300

    Camera follow the mouse

---
+++ Assets/RW/Scripts/CameraFollow.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    public GameObject targetObject;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        float targetObjectX = targetObject.transform.position.x;
+        Vector3 newCameraPosition = transform.position;
+        newCameraPosition.x = targetObjectX;
+        transform.position = newCameraPosition;
+    }
+}
